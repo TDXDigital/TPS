a47aa28875051b46b37f56c48413a92494dfcd31
FireByTrial <James.oliver59@gmail.com>
2013-11-21 18:00:19 -0500

    Create DB_Auth.php

---
+++ Security/DB_Auth.php
@@ -0,0 +1,11 @@
+// TUTORIAL
+// http://www.wikihow.com/Create-a-Secure-Login-Script-in-PHP-and-MySQL
+//
+// THIS CODE IS INTENDED TO AUTHENTICATE A USER AGAINST A DB HOSTED AUTHLIST
+//
+//
+<?php
+function DB_Auth ($user, $password, $xml_server) {
+    
+}
+?>
