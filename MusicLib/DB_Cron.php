1a5494936e1e6b022ae82f85be176a66b2c01baf
FireByTrial <James.oliver59@gmail.com>
2013-11-30 00:56:47 -0700

    added database search initial files for EPV2

---
+++ MusicLib/DB_Cron.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+$json_arr=array();
+$artist = addslashes($_GET['A']);
+$title = $_GET['T'];
+$link = mysqli_connect($_SESSION['DBHOST'],$_SESSION['usr'],$_SESSION['rpw'],$_SESSION['DBNAME']) or die("Connection Error");
+$query_artist = "SELECT artist, album FROM song where Artist REGEXP '$artist' group by soundex(artist)";
+$result=mysqli_query($link,$query_artist);
+if(mysqli_error($link)){
+    echo mysqli_error($link);
+}
+while($row = mysqli_fetch_array($result)){
+    //echo $row['artist'] ."<br/>";
+    array_push($json_arr,$row['artist']);
+}
+/*foreach (mysqli_fetch_array($result) as $row){
+    echo $row['artist']."<br/>";
+}*/
+echo json_encode($json_arr);
+//echo "<h3>Complete:$artist</h3>";
+$result->free();
+$link->close();
+//echo "$artist";
+?>
