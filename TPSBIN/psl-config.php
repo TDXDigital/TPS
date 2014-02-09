5c6c01f3b14eda375548360a70c56951a3cb00ac
FireByTrial <James.oliver59@gmail.com>
2014-02-09 00:19:29 -0700

    Update psl-config.php

--- TPSBIN/psl-config.php
+++ TPSBIN/psl-config.php
@@ -4,8 +4,8 @@
  * http://www.wikihow.com/Create-a-Secure-Login-Script-in-PHP-and-MySQL
  */  
 define("HOST", "172.22.10.10");     // The host you want to connect to.
-define("USER", "root");    // The database username. 
-define("PASSWORD", "88.3ForLife");    // The database password. 
+define("USER", "program");    // The database username. 
+define("PASSWORD", "pirateradio");    // The database password. 
 define("DATABASE", "ckxu");    // The database name.
  
 define("CAN_REGISTER", "any");
