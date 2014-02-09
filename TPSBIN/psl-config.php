94284410ba47b868d1094c460ee0b42ddd3dd33d
James Oliver <j.oliver@uleth.ca>
2014-02-08 22:16:32 -0700

    began implementing secure login

---
+++ TPSBIN/psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "program");    // The database username. 
+define("PASSWORD", "pirateradio");    // The database password. 
+define("DATABASE", "ckxu");    // The database name.
+ 
+define("CAN_REGISTER", "restricted");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
