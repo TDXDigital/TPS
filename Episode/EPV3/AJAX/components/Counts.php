e5bb2723e0085bd6705d63ca3507fdf29710652b
FireByTrial <James.oliver59@gmail.com>
2014-02-25 01:08:03 -0700

    Added Percent to TPS 0.3

--- Episode/EPV3/AJAX/components/Counts.php
+++ Episode/EPV3/AJAX/components/Counts.php
@@ -1,6 +1,7 @@
 <?php
     include_once "../../../../TPSBIN/functions.php";
     sec_session_start();
+    $DEBUG = FALSE;
 	
 	function to12hour($hour1){ 
 		// 24-hour time to 12-hour time 
@@ -55,7 +56,11 @@ else if($con){
 	}
 
     // GET SYSTEM SETTINGS
-	$SETTA = mysql_query("SELECT * FROM station,program where station.callsign = program.callsign and program.callsign = '".$CALL."' and program.programname='".$SHOW."'");
+	$SETTA = mysql_query("SELECT * FROM station,program,genre where station.callsign = program.callsign and program.callsign = '".$CALL."' and program.programname='".$SHOW."' and program.genre=genre.genreid");
+    if($DEBUG){
+        echo "SELECT * FROM station,program,genre where station.callsign = program.callsign and program.callsign = '".$CALL."' and program.programname='".$SHOW."' and program.genre=genre.genreid
+        ";
+    }
     if(sizeof($SETTA)===1){
         $SETTINGS = mysql_fetch_array($SETTA);
     }
@@ -82,17 +87,17 @@ else if($con){
 	
 		$SQLProg = "SELECT Genre.*, Program.length from Genre, Program where Program.programname=\"" . $SHOW . "\" and program.callsign=\"" . $CALL . "\" and Program.genre=Genre.genreid";
 		if(!($result = mysql_query($SQLProg))){
-			echo "Program Error 001 " . mysql_error();
+			die("Program Error 001 " . mysql_error());
 		}
 		if(!($Requirements = mysql_fetch_array($result))){
-			echo "Program Error 002 " . mysql_error();
+			die("Program Error 002 " . mysql_error());
 		}
 		$SQL2PR = "SELECT * from Program where programname=\"" . $SHOW . "\" and callsign=\"" . $CALL . "\" ";
 		if(!($result2 = mysql_query($SQL2PR))){
-			echo "Program Error 003 " . mysql_error();
+			die("Program Error 003 " . mysql_error());
 		}
 		if(!($Req2 = mysql_fetch_array($result2))){
-			echo "Program Error 004 " . mysql_error();
+			die("Program Error 004 " . mysql_error());
 		}
 		
 		if($Req2['CCX']!='-1'){
@@ -117,12 +122,27 @@ else if($con){
 		}
 		
 		// COUNT CANCON
-		$SQLCOUNTCC = "Select songid from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and cancon='1' ";
-		$resultCC = mysql_query($SQLCOUNTCC);
-		$RECCC = mysql_num_rows($resultCC);
-		
+		$SQLCOUNT = "SELECT (Select count(*) from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and cancon='1') AS CanCon,
+        (Select count(*) from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and hit='1') AS Hits,
+        (Select count(*) from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and category='51' and AdViolationFlag is NULL) AS Ads,
+        (Select count(*) from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and playlistnumber IS NOT NULL) AS Playlist,
+        (Select count(*) from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and (category like '2%' or category like '3%')) AS Songs,
+        (Select count(*) from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and category='45') AS Promo,
+        (Select count(*) from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and category like '1%' and title like '%PSA%' ) AS PSA,
+        (Select round(((Playlist / Songs)*100),1)) AS Playlist_Percent,
+        (Select round(((CanCon / Songs)*100),1)) AS CanCon_Percent";
+		if( $DEBUG){
+            echo $SQLCOUNT;
+        }
+        if(!$result = mysql_query($SQLCOUNT)){
+		    die("Program Error 006: " . mysql_error());
+		}
+		if(!$REC = mysql_fetch_array($result)){
+		    die("Program Errror 007: ".mysql_error());
+		}
+		/*
 		// COUNT HITS
-		$SQLCOUNTHI = "Select songid from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and hit='1' ";
+		$SQLCOUNTHI = "Select count from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and hit='1' ";
 		$resultHI = mysql_query($SQLCOUNTHI);
 		$RECHI = mysql_num_rows($resultHI);
 		
@@ -143,14 +163,14 @@ else if($con){
 		$SQLCOUNTPROMO = "Select songid from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and category='45'";
 		$SQLCOUNTPSA = "Select songid from SONG where callsign='" . $CALL . "' and programname='" . $SHOW . "' and date='" . $DATE . "' and starttime='" . $START . "' and category like '1%' and title like '%PSA%' ";
 		//$SQLCOUNTPSA = "Select songid from SONG where callsign='" . addslashes($CALLSHOW) . "' and programname='" . addslashes($_POST['program']) . "' and date='" . addslashes($_POST['user_date']) . "' and starttime='" . addslashes($_POST['user_time']) . "' and category like '1%' and title like '%Promo%' ";
-		
+		*/
 		$resultPSA = mysql_query($SQLCOUNTPSA);
 		$resultPROMO = mysql_query($SQLCOUNTPROMO);
 		$RECPSA = mysql_num_rows($resultPROMO);
 		$RECPSA += mysql_num_rows($resultPSA);
 		
-		$ADREQ = ceil(1 * $Requirements['length'] / 60);
-		$PSAREQ = ceil(2 * $Requirements['length'] / 60);
+		$ADREQ = ceil($SETTINGS['ST_ADSH'] * $SETTINGS['length'] / 60);
+		$PSAREQ = ceil($SETTINGS['ST_PSAH'] * $SETTINGS['length'] / 60);
 		$HITLIM = $Req2['HitLimit'];
 		
 		// NEW STYLE CODE
@@ -159,33 +179,83 @@ else if($con){
 		/*$ccf = "SELECT count(songid) from song where callsign='".$CALL."' and starttime='".$START."' and programname='".$SHOW."' and date='".$DATE."' and cancon='1'";
 		$resultarrcf = mysql_query($ccf);
 		$ccfarr = mysql_fetch_array($resultarrcf);*/
-		echo "<div id='ccfill'>".$RECCC."</div>";
+        if($SETTINGS['CCType']==0){
+            echo "<div id='ccfill'>".$REC['CanCon_Percent']."</div>";
+            echo "<div id='ccreq'>";
+            echo ($SETTINGS['canconperc']*100)."%";
+            echo "</div>";
+            echo "<div id='ccpass'>";
+		    if(($SETTINGS['canconperc']*100)>$REC['CanCon_Percent']){
+			    echo "0";
+		    }
+		    else{
+			    echo "1";
+		    }
+		    echo "</div>";
+        }
+        else{
+            echo "<div id='ccfill'>".$REC['CanCon']."</div>";
+            echo "<div id='ccreq'>".$CC."</div>";
+            echo "<div id='ccpass'>";
+		    if($CC>$REC['CanCon']){
+			    echo "0";
+		    }
+		    else{
+			    echo "1";
+		    }
+		    echo "</div>";
+        }
+		/*
+		echo "<div id='ccfill'>".$REC['CanCon']."</div>";
 		echo "<div id='ccreq'>".$CC."</div>";
 		echo "<div id='ccpass'>";
-		if($CC>$RECCC){
+		if($CC>$REC['CanCon']){
 			echo "0";
 		}
 		else{
 			echo "1";
 		}
-		echo "</div>";
-        echo "<div id='pltype'>PERC";
+		echo "</div>";*/
+        echo "<div id='pltype'>";
+        echo $SETTINGS['PlType'];
         echo "</div>";
-		echo "<div id='plfill'>".$RECPL."</div>";
-		echo "<div id='plreq'>".$PL."</div>";
-		echo "<div id='plpass'>";
-		if($PL>$RECPL){
-			echo "0";
-		}
-		else{
-			echo "1";
-		}
-		echo "</div>";
-		echo "<div id='adreq'>".$REC51."</div>";
-		echo "<div id='adfill'>".$ADREQ."</div>";
-		echo "<div id='hithas'>".$RECHI."</div>";
+        echo "<div id='cctype'>";
+        echo $SETTINGS['CCType'];
+        echo "</div>";
+		
+		
+        if($SETTINGS['PlType']==0){
+            echo "<div id='plfill'>".$REC['Playlist_Percent']."</div>";
+            echo "<div id='plreq'>";
+            echo ($SETTINGS['playlistperc']*100)."%";
+            echo "</div>";
+            echo "<div id='plpass'>";
+		    if((($SETTINGS['playlistperc'])*100)>$REC['Playlist_Perc']){
+			    echo "0";
+		    }
+		    else{
+			    echo "1";
+		    }
+		    echo "</div>";
+        }
+        else{
+            echo "<div id='plfill'>".$REC['Playlist']."</div>";
+            echo "<div id='plreq'>".$PL."</div>";
+            echo "<div id='plpass'>";
+		    if($PL>$REC['Playlist']){
+			    echo "0";
+		    }
+		    else{
+			    echo "1";
+		    }
+		    echo "</div>";
+        }
+		
+		echo "<div id='adreq'>".$ADREQ."</div>";
+		echo "<div id='adfill'>".$REC['Ads']."</div>";
+		echo "<div id='hithas'>".$REC['Hits']."</div>";
 		echo "<div id='hitlim'>".$HITLIM."</div>";
-		echo "<div id='psafill'>".$RECPSA."</div>";
+		echo "<div id='psafill'>".$REC['PSA']."</div>";
 		echo "<div id='psareq'>".$PSAREQ."</div>";
 }
 else{
