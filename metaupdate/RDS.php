b5b67abb3a58ba5132e74566688cde4fe9e9f043
FireByTrial <James.oliver59@gmail.com>
2014-03-04 22:14:19 -0700

    another stupid error.........

--- metaupdate/RDS.php
+++ metaupdate/RDS.php
@@ -137,7 +137,7 @@ order by song.time desc, episode.starttime desc limit 1;");
                 $http_status = curl_getinfo($mysession, CURLINFO_HTTP_CODE);
                 curl_close($mysession);
                 echo "Completed with code $http_status<br/>";
-                $dbl->query("INSERT INTO `rds` (`rds_status`,`value`,`server`) values (".mysqli_escape_string($http_status).",'".mysqli_escape_string($data),"',".mysqli_escape_string($Server['Host'].':'.$Server['Port'])."')");
+                $dbl->query("INSERT INTO `rds` (`rds_status`,`value`,`server`) values (".mysqli_escape_string($http_status).",'".mysqli_escape_string($data),"','".mysqli_escape_string($Server['Host'].':'.$Server['Port'])."')");
         }
         echo "<strong>Updates Finished Disconnecting...</strong></br>";
     }
