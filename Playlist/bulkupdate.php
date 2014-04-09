882ebd631d1bd2a5d5b2b601c4ef2c6cd8386d04
FireByTrial <James.oliver59@gmail.com>
2014-04-08 23:57:08 -0600

    updated library upload

--- Playlist/bulkupdate.php
+++ Playlist/bulkupdate.php
@@ -3,14 +3,25 @@
     sec_session_start();
 ?>
 <html>
+    <head>
+        <script src="../js/jquery/js/jquery-2.0.3.min.js" ></script>
+        <script src="../js/jquery/js/jquery-ui-1.10.0.custom.min.js" ></script>
+        <link href="../js/jquery/js/css/trontastic/jquery-ui-1.10.0.custom.min.css"/>
+        <meta title="Upload Playlist"/>
+    </head>
 <body>
-
-<form action="upload_playlist.php" method="post"
+    <script>
+        function submitted(){
+            $("#processing").show();
+        }
+    </script>
+<form action="upload_playlist.php" method="post" onsubmit="submitted();"
 enctype="multipart/form-data">
 <label for="file">Filename:</label>
 <input type="file" name="file" id="file"><br>
 <input type="submit" name="submit" value="Submit">
 </form>
+    <div id="processing" style="display: none;"><progress></progress>Working...</div>
 
 </body>
 </html>
--- Playlist/upload_playlist.php
+++ Playlist/upload_playlist.php
@@ -1,5 +1,9 @@
 <?php
 include_once "../TPSBIN/functions.php";
+include_once "../includes/phpexcel/Classes/PHPExcel.php";
+include_once "../TPSBIN/functions.php";
+include_once "../TPSBIN/db_connect.php";
+include "../includes/phpexcel/Classes/PHPExcel/IOFactory.php";
 sec_session_start();
 
 if ($_FILES["file"]["error"] > 0)
@@ -11,6 +15,56 @@ else
   echo "Upload: " . $_FILES["file"]["name"] . "<br>";
   echo "Type: " . $_FILES["file"]["type"] . "<br>";
   echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+  $inputFileName = $_FILES["file"]["tmp_name"];
+  echo "Stored in: " . $inputFileName;
   }
+
+  //  Read your Excel workbook
+try {
+    $inputFileType = PHPExcel_IOFactory::identify($inputFileName);
+    $objReader = PHPExcel_IOFactory::createReader($inputFileType);
+    $objPHPExcel = $objReader->load($inputFileName);
+} catch(Exception $e) {
+    die('Error loading file "'.pathinfo($inputFileName,PATHINFO_BASENAME).'": '.$e->getMessage());
+}
+//echo "Start EXCEL PRINT</br><table>";
+
+
+//  Get worksheet dimensions
+$sheet = $objPHPExcel->getSheet(2); 
+$highestRow = $sheet->getHighestRow(); 
+$highestColumn = $sheet->getHighestColumn();
+
+//  Loop through each row of the worksheet in turn
+for ($row = 1; $row <= $highestRow; $row++){ 
+    //  Read a row of data into an array
+    $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row,
+                                    NULL,
+                                    TRUE,
+                                    FALSE);
+    //  Insert row data array into your database of choice here
+    /*echo "<tr>";
+    echo "<td>".$rowData[0][0]."</td>";
+    //print_r($rowData);
+    echo "<td>".$rowData[0][1]."</td>";
+    echo "<td>".$rowData[0][2]."</td>";
+    echo "<td>".$rowData[0][3]."</td>";
+    echo "<td>".$rowData[0][4]."</td>";
+    echo "<td>".$rowData[0][5]."</td><td>";*/
+    //echo "<td>".$rowData[1][0]."</td>";
+    
+
+    $stmt = $mysql->prepare("INSERT IGNORE INTO `library` (artist, album, datein, year) VALUES (?, ?, ?, ?)");
+    $stmt->bindparam('sssi',$rowData[0][0],$rowData[0][1],$rowData[0][3],$rowData[0][4]); // LABEL REMOVED
+    $stmt->execute();
+    var_dump($stmt); // shows null values
+    var_dump($stmt->errno); // note literal, displays value 
+    //echo $stmt->error();
+    printf("%d Row inserted.\n", $stmt->affected_rows);
+    $stmt->close();
+    //echo "</td></tr>";
+}
+//echo "</table>";
+echo "Complete";
+
 ?>
