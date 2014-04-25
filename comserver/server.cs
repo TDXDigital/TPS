8708555b2726a804d57c78e351533980b4da98dd
FireByTrial <James.oliver59@gmail.com>
2014-04-25 13:00:36 -0600

    Update server.cs

--- comserver/server.cs
+++ comserver/server.cs
@@ -1,3 +1,11 @@
+//http://tech.pro/tutorial/704/csharp-tutorial-simple-threaded-tcp-server
+/* This server is designed to continuosly listen to a TCP destination for data to be broadcasted
+ * alternative design is to have system listen for TCP and have access to com port
+ * that will essentially interupt the transmission to upload to serveer and then continue with 
+ * broadcast of com information to TCP
+ *
+ */
+
 using System;
 using System.Text;
 using System.Net.Sockets;
