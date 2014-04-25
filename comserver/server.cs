f05e179a5b859bab122aef2c580c62ebf9e74be6
FireByTrial <James.oliver59@gmail.com>
2014-04-25 12:56:41 -0600

    Create server.cs

---
+++ comserver/server.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace TCPServerTutorial
+{
+  class Server
+  {
+    private TcpListener tcpListener;
+    private Thread listenThread;
+
+    public Server()
+    {
+      this.tcpListener = new TcpListener(IPAddress.Any, 3000);
+      this.listenThread = new Thread(new ThreadStart(ListenForClients));
+      this.listenThread.Start();
+    }
+  }
+}
