51ec09d13da8bf24b273ca26b211bbfa93142acc
FireByTrial <FireByTrial@users.noreply.github.com>
2015-01-09 12:53:41 -0700

    Update db_connect.php

--- TPSBIN/db_connect.php
+++ TPSBIN/db_connect.php
@@ -1,8 +1,14 @@
 <?php
 include_once 'psl-config.php';   // As functions.php is not included
 //echo constant("HOST");
+if(!defined("HOST") || !defined("USER") || !defined("PASSWORD") || !defined("DATABASE")){
+    session_destroy();
+    header('location: /Security/login.php?e=invalid%20params');
+}
+
 if(!$mysqli = new mysqli(constant("HOST"), constant("USER"), constant("PASSWORD"), constant("DATABASE"))){
+    //header('location: /Security/login.php?e=database%20access%20denied');
+    session_destroy();
     header('location: /Security/login.php?e=database%20access%20denied');
-    
 }
 ?>
