db52bea7377d6400aaa002f3e892ae4b205077d8
FireByTrial <FireByTrial@users.noreply.github.com>
2015-01-26 16:13:26 -0700

    Update p2insertstation.php

--- station/p2insertstation.php
+++ station/p2insertstation.php
@@ -19,7 +19,7 @@ if($_POST[callsign]!="" && $_POST[name]!=""){
   }
   else
   {
-    $err = $mysqli->errno();
+    $err = $mysqli->error;
     if($err == 1062)
     {
       echo "<p>Station name " . $_POST[callsign] . " already exists</p>";
@@ -27,7 +27,7 @@ if($_POST[callsign]!="" && $_POST[name]!=""){
     else
     {
       echo $err;
-      //echo mysql_error();
+      echo mysqli->error;
       echo $sql;
     }
   }
