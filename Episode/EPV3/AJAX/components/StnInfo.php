a240872272c4cb834b1ede5ce09fea69a20e88c2
FireByTrial <FireByTrial@users.noreply.github.com>
2015-01-27 20:04:28 -0700

    Update StnInfo.php

--- Episode/EPV3/AJAX/components/StnInfo.php
+++ Episode/EPV3/AJAX/components/StnInfo.php
@@ -1,4 +1,7 @@
 <?php
-	session_start();
+	include_once "../../../TPSBIN/functions.php";
+	if(!is_session_started()){
+		sec_session_start();
+	}
 	echo "<p>Station: " . strtoupper($_SESSION['callsign']) . "</p>"; 
 ?>
