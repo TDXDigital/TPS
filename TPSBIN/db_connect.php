38459d493c6131c13c1c7da01c7e5e4e3e0e6609
FireByTrial <FireByTrial@users.noreply.github.com>
2015-01-30 16:00:47 -0700

    Revert "Apache mods"

--- TPSBIN/db_connect.php
+++ TPSBIN/db_connect.php
@@ -1,7 +1,4 @@
 <?php
-include_once "../CONFIG.php";
-date_default_timezone_set($timezone);
-
 //echo constant("HOST");
 if(!defined("HOST") || !defined("USER") || !defined("PASSWORD") || !defined("DATABASE")){
     session_destroy();
@@ -11,12 +8,6 @@ if(!defined("HOST") || !defined("USER") || !defined("PASSWORD") || !defined("DAT
     echo "<br>DATABASE:".constant("DATABASE");
     echo "<br><br><a href=/Security/login.html?e=invalid%20params>Return to login</a>";
     //header('location: /Security/login.html?e=invalid%20params');
-}
-elseif(!isset($_SESSION['DBHOST'])&&$legacy){
-    $_SESSION['DBHOST']=constant("HOST");
-    $_SESSION['usr']=constant("USER");
-    $_SESSION['rpw']=constant("PASSWORD");
-    $_SESSION['DBNAME']=constant("DATABASE");
 }
 
 include_once 'psl-config.php';   // As functions.php is not included
