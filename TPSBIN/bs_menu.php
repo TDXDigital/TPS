a4df1176388f1da6de2c2f8f846ab0803add6082
FireByTrial <FireByTrial@users.noreply.github.com>
2015-02-27 08:02:34 -0700

    Update bs_menu.php

--- TPSBIN/bs_menu.php
+++ TPSBIN/bs_menu.php
@@ -21,7 +21,11 @@
     // CONNECT TO DB
 
     // QUERY "Permissions
-    if($stmt = $mysqli->prepare("SELECT * FROM permissions WHERE callsign=? and access=?")){
+    if($stmt = $mysqli->prepare("SELECT Staton_Settings_View,Station_Settings_Edit, "
+        ."Member_View,Member_Edit,Member_Create,Program_View,Program_Edit,Program_Create,"
+        ."Genre_View,Genre_Edit,Genre_Create, Playlist_View,Playlist_Edit,Playlist_Create,"
+        ."Library_View,Library_Edit,Library_Create,Advert_View,Advert,Edit,Advert_Create,"
+        ."Audit_View FROM permissions WHERE callsign=? and access=?")){
         // Bind DBNAME and access
         $stmt->bind_param("si",$dbname,$access);
         //query
