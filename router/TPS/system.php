b3785c751192a9ac465ff6fd65cd215b466082d5
James Oliver <james.oliver59@gmail.com>
2015-07-12 14:59:02 -0600

    moved TPS to router

---
+++ router/TPS/system.php
@@ -0,0 +1,14 @@
+<?php
+namespace TPS;
+
+Class system{
+    protected $var;
+            
+    public function __construct(){
+        
+    }
+    
+    public function logout(){
+        
+    }
+}
