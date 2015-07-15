7f68ab835234f55ec8cccb214eb95ac42207a765
James <FireByTrial@users.noreply.github.com>
2015-07-14 20:07:59 -0600

    Update index.php

--- public/index.php
+++ public/index.php
@@ -12,16 +12,11 @@
     require($path);
 
     $router = new \TPS\Router;
-    print "<br>"."Loaded Router";
     $routes = array(
         '/' => '',
         '/test/:title' => 'Main:test@get',
         '/login'=>'Main:login@get'
     );
-    print "<br>"."Loaded Routes Array";
     $router->addRoutes($routes);
-    print "<br>"."Set Routes";
     $router->set404Handler("Main:notFound");
-    print "<br>"."Added 404 Handeler";
     $router->run();
-    print "<br>"."Ran Run";
