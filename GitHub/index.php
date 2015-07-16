eb6b5070c884157e28624e9afe785cc11d55ce11
James Oliver <james.oliver59@gmail.com>
2015-07-15 22:56:18 -0600

    ...

--- GitHub/index.php
+++ GitHub/index.php
@@ -2,18 +2,20 @@
 
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
-if ( isset($_POST['payload']) ) {
-    shell_exec( 'cd /var/www/html/ckxu.uleth.ca/public_html/ && git fetch --all && git pull' );
-    $headers = array();
-    foreach($_SERVER as $key => $value) {
-        if (substr($key, 0, 5) <> 'X-Git') {
-            continue;
-        }
-        $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
-        $headers[$header] = $value;
+$headers = array();
+foreach($_SERVER as $key => $value) {
+    if (substr($key, 0, 5) <> 'X-Git') {
+        continue;
     }
-    error_log("Recieved GitHub push with ID ".$headers['X-Github-Delivery'].
-            " RE:".$headers['X-Github-Event']);
+    $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+    $headers[$header] = $value;
+}
+error_log("Recieved GitHub push with ID ".$headers['X-Github-Delivery'].
+        " RE:".$headers['X-Github-Event']);
+
+if ( isset($_POST['payload']) || isset($headers['X-Github-Delivery']) ) {
+    shell_exec( 'cd /var/www/html/ckxu.uleth.ca/public_html/ && git fetch --all && git pull' );
+    
 }
 
 ?>hi
