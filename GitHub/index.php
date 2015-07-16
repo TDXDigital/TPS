a164ac1948d97549f8bcc29ff4fb5311541687f6
James Oliver <james.oliver59@gmail.com>
2015-07-15 23:04:43 -0600

    ,.,,

--- GitHub/index.php
+++ GitHub/index.php
@@ -14,7 +14,7 @@ error_log("Recieved GitHub push with ID ".$headers['X-Github-Delivery'].
         " RE:".$headers['X-Github-Event']);
 
 if ( isset($_POST['payload']) || isset($headers['X-Github-Delivery']) ) {
-    shell_exec( 'cd /var/www/html/ckxu.uleth.ca/public_html/ && git fetch --all && git pull' );
+    shell_exec( 'cd /var/www/html/ckxu.uleth.ca/public_html/ && git fetch --all && git reset --hard origin/templateEngine && git pull' );
     
 }
 
