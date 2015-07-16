00934dfc1c652f4b0435d626acde490db1a9a8db
James Oliver <james.oliver59@gmail.com>
2015-07-15 19:01:45 -0600

    add webhook

---
+++ GitHub/webhook.php
@@ -0,0 +1,9 @@
+<?php
+
+// Use in the "Post-Receive URLs" section of your GitHub repo.
+
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /var/www/ckxu.uleth.ca/public_html/ && git fetch --all && git pull' );
+}
+
+?>hi
