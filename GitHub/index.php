11a2370dcdbe5436d950a7cbd16eb4c6e6558fea
James Oliver <james.oliver59@gmail.com>
2015-07-15 21:01:02 -0600

    fix webhook path

--- GitHub/index.php
+++ GitHub/index.php
@@ -3,7 +3,17 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( isset($_POST['payload']) ) {
-  shell_exec( 'cd /var/www/html/ckxu.uleth.ca/public_html/ && git fetch --all && git pull' );
+    shell_exec( 'cd /var/www/html/ckxu.uleth.ca/public_html/ && git fetch --all && git pull' );
+    $headers = array();
+    foreach($_SERVER as $key => $value) {
+        if (substr($key, 0, 5) <> 'X-Git') {
+            continue;
+        }
+        $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+        $headers[$header] = $value;
+    }
+    error_log("Recieved GitHub push with ID ".$headers['X-Github-Delivery'].
+            " RE:".$headers['X-Github-Event']);
 }
 
 ?>hi
