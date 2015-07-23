9d2f2743ac53cc6b345a2158205bae9b87b3810b
James Oliver <james.oliver59@gmail.com>
2015-07-23 01:18:08 -0600

    add example file, move settings

--- public/header.php
+++ public/header.php
@@ -5,25 +5,7 @@
 #specify the system temp (linux likely needs /tmp due to SELinux
 //$ConfigTemp = $basepath.DIRECTORY_SEPARATOR."temp";
 
-if(file_exists('slimConfig.php')){
-    require_once('slimConfig.php');
-}    
-
-// Set variables
-$debug = TRUE;
-$basepath = dirname(__DIR__).DIRECTORY_SEPARATOR;
-$autoload_path = $basepath."vendor".DIRECTORY_SEPARATOR."autoload.php";
-$twig_path = $basepath."lib".DIRECTORY_SEPARATOR."Twig".DIRECTORY_SEPARATOR
-        ."Autoloader.php";
-$slim_path = $basepath."lib".DIRECTORY_SEPARATOR."Slim".DIRECTORY_SEPARATOR
-        ."Slim.php";
-#$Views_path = $basepath.DIRECTORY_SEPARATOR."Views";
-/*$UserViews_path = $basepath.DIRECTORY_SEPARATOR."Views"
-        .DIRECTORY_SEPARATOR."User";
-$SystemViews_path = $basepath.DIRECTORY_SEPARATOR."Views"
-        .DIRECTORY_SEPARATOR."System";*/
-$views_path = $basepath.DIRECTORY_SEPARATOR."Views";
-$temp_path = $Config_Temp?:$basepath.DIRECTORY_SEPARATOR."temp";
+require_once('slimConfig.php');
 
 //load twig
 if(file_exists($autoload_path)){
