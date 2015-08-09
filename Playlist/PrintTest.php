5768fac4a2b1e3b406c15ff39c3c7f798d03d64d
James <FireByTrial@users.noreply.github.com>
2015-08-08 20:41:44 -0600

    Update PrintTest.php

--- Playlist/PrintTest.php
+++ Playlist/PrintTest.php
@@ -90,7 +90,7 @@
                 echo "<img style='float: right; margin: 0px;' width='25px' src='./pointer.png' alt='PRO'/>";
             }
             substr("abcdef", -1);
-            if(strlen($artist)>16){
+            if(strlen($artist)>18){
                 $artpost = "...";
             }
             else{
@@ -102,7 +102,7 @@
             else{
                 $albpost = "";
             }
-            echo "</span><br style='clear: both'><strong style='float: left'>".substr($artist,0,16).$artpost."</strong><br><i style='float:left'>".substr($album,0,18).$albpost."</i><span style='float:right;'>$genre</span><br style='clear: both'/></div>";
+            echo "</span><br style='clear: both'><strong style='float: left'>".substr($artist,0,18).$artpost."</strong><br><i style='float:left'>".substr($album,0,18).$albpost."</i><span style='float:right;'>$genre</span><br style='clear: both'/></div>";
         }
     }
     else{
