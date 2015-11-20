4ec90618471d3c70ab945b1f1a8faf7188ca5445
James Oliver <james.oliver59@gmail.com>
2015-11-20 01:26:15 -0700

    rewrite Reports module

--- public/lib/episode.php
+++ public/lib/episode.php
@@ -34,13 +34,20 @@ namespace TPS;
 
 require_once 'program.php';
 class episode extends program{
+    private $needsUpdate = True;
     protected $program = null;
     protected $EpisodeID = null;
     protected $time = null;
     protected $date = null;
-    public $type = 0;
-    public $recordDate = null;
-    public $description = null;
+    protected $originIP = null;
+    protected $type = 0;
+    protected $recordDate = null;
+    protected $description = null;
+    protected $endTime = null;
+    protected $locked = false;
+    protected $totalSpokenTime = null;
+    protected $finalizedTimestamp = null;
+    protected $lastAccessTimestamp = null;
     /**
      * Either ID or date, time must be provided
      * @global type $mysqli
@@ -70,34 +77,56 @@ class episode extends program{
         
     }
     
-    protected function getEpisode(){
-        if($stmt = $this->mysqli->prepare("SELECT"
-                . "callsign, programname, date, starttime, type,"
-                . "prerecorddate, description, IP_Created FROM episode "
+    public function date($date=Null){
+        $this->date = $date;
+        return $this->$date;
+    }
+    
+    public function getEpisode(){
+        if($this->needsUpdate){
+            $stmt = $this->mysqli->prepare("SELECT "
+                . "callsign, programname, date, starttime, type, "
+                . "prerecorddate, description, IP_Created, totalspokentime, "
+                . "`Lock`, EndStamp, LastAccess FROM episode "
                 . "WHERE `EpNum`=? or (`programname`=? and `date`=? and "
-                . "`starttime`=? and `callsign`=?) order by programID desc")){
+                . "`starttime`=? and `callsign`=?)");
+            if($stmt === false){
+                $this->log->error($this->mysqli->error,"getEpisode");
+            }
             $stmt->bind_param("issss",$this->EpisodeID, $this->program->name,
                 $this->date, $this->time, $this->callsign);
+            $stmt->bind_result($this->callsign,$this->name,$this->date,
+                    $this->time, $this->type, $this->recordDate, 
+                    $this->description, $this->originIP, $this->totalSpokenTime,
+                    $this->locked, $this->finalizedTimestamp, 
+                    $this->lastAccessTimestamp);
             if($stmt->execute()){
-                $stmt->store_result();
+                //$stmt->store_result();
                 if($stmt->num_rows > 1){
                     throw new Exception(
                             "Too episode many results, non unique results");
-                    // @todo return result in array
-                    // @todo implement updateEpisode public function
                 }
-                $this->EpisodeID=$this->mysqli->insert_id;
+                $stmt->fetch();
+                // @todo return result in array
+                // @todo implement updateEpisode public function
             }
             else{
                 $this->log->error($this->mysqli->errno);
             }
             $stmt->close();
         }
-        else{
-            $this->log->error("Failed to create new episode"
-                    . $this->mysqli->error);
-            return false;
-        }
+        return array(
+            "id" => $this->EpisodeID,
+            "name" => $this->name,
+            "date" => $this->date,
+            "time" => $this->time,
+            "type" => $this->type,
+            "endTime" => $this->endTime,
+            "callsign" => $this->callsign,
+            "recordDate" => $this->recordDate,
+            "description" => $this->description,
+            "totalSpokenTime" => $this->totalSpokenTime,
+        );
     }
     
     public function createEpisode(){
