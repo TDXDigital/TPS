49b2fac23009153b564b32137e5a62a69019b7ec
FireByTrial <James.oliver59@gmail.com>
2015-12-01 21:56:31 -0700

    check for empty list

--- Playlist/PrintTest.php
+++ Playlist/PrintTest.php
@@ -75,7 +75,7 @@
         foreach($_SESSION['PRINTID'] as $BCD){
             $albums = $library->getAlbumByRefcode($BCD['RefCode'], TRUE);
             if(sizeof($albums)<1){
-                break;
+                continue;
             }
             $albumArr = $albums[0];
             $RefCode = $albumArr['RefCode'];
