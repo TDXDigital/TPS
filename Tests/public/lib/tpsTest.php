8780f430ea118cd49957ad8241e93be1b746ce60
FireByTrial <James.oliver59@gmail.com>
2016-01-12 23:34:58 -0700

    add test for sanitize pagination

--- Tests/public/lib/tpsTest.php
+++ Tests/public/lib/tpsTest.php
@@ -19,7 +19,9 @@ class TPSTest extends \PHPUnit_Extensions_Database_TestCase {
      * This method is called before a test is executed.
      */
     protected function setUp() {
-        $this->object = new \TPS\TPS(FALSE,FALSE);
+        $GLOBALS['db'] = $this->getConnection();
+        $this->object = new \TPS\TPS(FALSE,FALSE,NULL,
+                dirname(__FILE__).'/_files/DBSETTINGS.xml');
     }
 
     /**
@@ -27,7 +29,7 @@ class TPSTest extends \PHPUnit_Extensions_Database_TestCase {
      * This method is called after a test is executed.
      */
     protected function tearDown() {
-        restore_include_path();
+        unset($GLOBALS['db']);
     }
 
     /**
@@ -45,12 +47,36 @@ class TPSTest extends \PHPUnit_Extensions_Database_TestCase {
      * @covers TPS\TPS::sanitizePagination
      */
     public function testSanitizePagination(){
+        // test standard values
         $page = 5;
         $max = 50;
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
-        
+        $this->object->sanitizePagination($page,$max);
+        $this->assertEquals(array($page,$max),array(199,250),
+                "standard test failed");
+        // test values out of bounds (9999), page > 0 is valid
+        $page = 500;
+        $max = 9999;
+        $this->object->sanitizePagination($page,$max);
+        $this->assertEquals(array($page,$max),array(498999,500000),
+                "bounds test failed");
+        // verify values that are not expected are ignored
+        $page = "string1";
+        $max = array("string",10);
+        $this->object->sanitizePagination($page,$max);
+        $this->assertEquals(array($page,$max),array(0,1000),
+                "non int test failed");
+        // test negatives
+        $page = -99999;
+        $max = -99999;
+        $this->object->sanitizePagination($page,$max);
+        $this->assertEquals(array($page,$max),array(0,1000), 
+                "negatives test failed");
+        // test zero values
+        $page = 0;
+        $max = 0;
+        $this->object->sanitizePagination($page,$max);
+        $this->assertEquals(array($page,$max),array(0,1000), 
+                "zero value test failed");
     }
     
     /**
--- public/lib/tps.php
+++ public/lib/tps.php
@@ -34,8 +34,7 @@ class TPS{
     protected $username;
     private $requirePDO;
     
-    private function getDatabaseConfig($target=NULL,
-            $xmlpath=NULL){
+    private function getDatabaseConfig($target=NULL,$xmlpath=NULL){
         if($xmlpath === NULL){
             $xmlpath = dirname(__FILE__).DIRECTORY_SEPARATOR."../../TPSBIN"
                     .DIRECTORY_SEPARATOR."XML"
@@ -86,24 +85,27 @@ class TPS{
                         $database["DBHOST"] = $server->IPV4;
                     }
                 }
-                $database["USER"] = easy_decrypt(ENCRYPTION_KEY,(string)$server->USER);
-                $database["PASSWORD"] = easy_decrypt(ENCRYPTION_KEY,(string)$server->PASSWORD);
+                $database["USER"] = easy_decrypt(
+                        ENCRYPTION_KEY,(string)$server->USER);
+                $database["PASSWORD"] = easy_decrypt(
+                        ENCRYPTION_KEY,(string)$server->PASSWORD);
                 $database["DATABASE"] = (string)$server->DATABASE;
+                $database["TYPE"] = $server->DBTYPE;
             }
         }
         return $database;
     }
     
     /**
-     * @access private
+     * @access public
      * @param int $pagination current page index
      * @param int $maxResult number of items to in response
      */
-    protected function sanitizePagination(&$pagination,&$maxResult){
-        if( !is_int($maxResult) || $maxResult > 1000):
+    public static function sanitizePagination(&$pagination,&$maxResult){
+        if( !is_int($maxResult) || $maxResult > 1000 || $maxResult<1):
             $maxResult = 1000;
         endif;
-        if( !is_int($pagination)):
+        if( !is_int($pagination) || $pagination<1):
             $pagination = 1;
         endif;
         $floor = abs(($pagination*$maxResult))-($maxResult+1);
@@ -119,7 +121,8 @@ class TPS{
         $maxResult = $ceil;
     }
 
-    public function __construct($enableDbReporting=FALSE, $requirePDO=FALSE) {
+    public function __construct($enableDbReporting=FALSE, $requirePDO=FALSE,
+            $settingsTarget=NULL, $settingsPath=NULL) {
         global $mysqli;
         global $db;
         $mysqli=$mysqli?:$GLOBALS['mysqli'];
@@ -128,16 +131,22 @@ class TPS{
         if(!$mysqli || !$db){
             // Establish DB connection
             $database = NULL;
-            if($database = $this->getDatabaseConfig()){
+            if($database = $this->getDatabaseConfig($settingsTarget,
+                    $settingsPath)){
+                if($database['TYPE']=="PDO"){
+                    $this->requirePDO = TRUE;
+                }
                 $databaseHost = $database['DBHOST'];
                 $databaseName = $database['DATABASE'];
                 if($this->requirePDO){
-                    $this->db = new \PDO("mysql:host=$databaseHost;dbname=$databaseName",
+                    $this->db = new \PDO(
+                            "mysql:host=$databaseHost;dbname=$databaseName",
                         $database['USER'], $database['PASSWORD']);
                     $this->mysqli = $this->db;
                 }
                 else{
-                    $this->db = new \PDO("mysql:host=$databaseHost;dbname=$databaseName",
+                    $this->db = new \PDO(
+                            "mysql:host=$databaseHost;dbname=$databaseName",
                         $database['USER'], $database['PASSWORD']);
                     $this->mysqli = new \mysqli(
                         $database['DBHOST'], 
@@ -154,11 +163,13 @@ class TPS{
                 }
             }
             else{
-                $this->mysqli = NULL;
+                $this->mysqli = $mysqli?:NULL;
+                $this->db = $db?:NULL;
             }
         }
         else{
             $this->mysqli = $mysqli;
+            $this->db = $db;
         }
         if(!$this->mysqliDriver){
             $this->mysqliDriver = new \mysqli_driver();
@@ -185,7 +196,8 @@ class TPS{
     public function getStations(){
         $callsign = null;
         $name = null;
-        if($con = $this->mysqli->prepare("SELECT callsign, stationname FROM station")){
+        if($con = $this->mysqli->prepare(
+                "SELECT callsign, stationname FROM station")){
             $stations = array();
             $con->execute();
             $con->bind_result($callsign,$name);
