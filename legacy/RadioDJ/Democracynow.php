dd917341f7c38927b78671a83987268bae48a76e
CKXU Radio Society <music@ckxu.com>
2018-01-10 19:18:33 -0700

    Update Democracynow.php
    Updating Democracy now link

--- legacy/RadioDJ/Democracynow.php
+++ legacy/RadioDJ/Democracynow.php
@@ -1,4 +1,4 @@
 <?php
-	header("location: http://traffic.libsyn.com/democracynow/dn".date("Y")."-".date("md")."-1.mp3")
+	header("location: http://traffic.libsyn.com/democracynow/dn".date("Y")."-".date("md").".mp3")
 	//"location: http://traffic.libsyn.com/democracynow/dn2012-0622-1.mp3"
 ?>
