bfb9b85501521e321956cb8a9bf9a9b41e1ecac6
Derek Melchin <melchin.derek@gmail.com>
2019-06-27 18:30:20 +0000

    Fix indentation

--- public/lib/playlist.php
+++ public/lib/playlist.php
@@ -281,11 +281,11 @@ class playlist extends TPS{
     * @return Array of associate arrays which contain the information of each album
     */
     public function getExpiredAlbums() {
-       $sql = $this->db->query("SELECT * FROM library WHERE RefCode IN (SELECT RefCode FROM playlist WHERE Expire < now());");
-       $expiredAlbums = [];
-       while ($row = $sql->fetch(\PDO::FETCH_ASSOC))
-           array_push($expiredAlbums, $row);
-       return $expiredAlbums;
+        $sql = $this->db->query("SELECT * FROM library WHERE RefCode IN (SELECT RefCode FROM playlist WHERE Expire < now());");
+        $expiredAlbums = [];
+        while ($row = $sql->fetch(\PDO::FETCH_ASSOC))
+            array_push($expiredAlbums, $row);
+        return $expiredAlbums;
    }
 
     /*
@@ -293,14 +293,14 @@ class playlist extends TPS{
     * @param int/array $refCode The album's unique playlist number. May be a single RefCode or a list of RefCodes.
     */
     public function moveAlbumToLibrary($refCode) {
-       if (!is_array($refCode))
-           $refCode = [$refCode];
-       if (sizeof($refCode) > 0) {
-           $refCodeList = "(" . implode(", ", $refCode) . ")";
-           $this->db->query("UPDATE playlist SET Expire=NOW() WHERE RefCode IN " . $refCodeList . ";");
-           $this->db->query("UPDATE library SET playlist_flag='FALSE' WHERE RefCode IN " . $refCodeList . ";");
-       }
-   }
+        if (!is_array($refCode))
+            $refCode = [$refCode];
+        if (sizeof($refCode) > 0) {
+            $refCodeList = "(" . implode(", ", $refCode) . ")";
+            $this->db->query("UPDATE playlist SET Expire=NOW() WHERE RefCode IN " . $refCodeList . ";");
+            $this->db->query("UPDATE library SET playlist_flag='FALSE' WHERE RefCode IN " . $refCodeList . ";");
+        }
+    }
 
 
     /*
@@ -311,18 +311,18 @@ class playlist extends TPS{
     *  to ensure the RefCode is stored for each entry in the song table.
     */
     public function setToMissing($refCode) {
-       $sql = $this->db->query("SELECT PlaylistId FROM playlist WHERE RefCode=" . $refCode . ";");
-       while ($row = $sql->fetch(\PDO::FETCH_ASSOC))
-           $playlistNumber = $row['PlaylistId'];
-       $this->db->query("UPDATE library SET missing=1 WHERE RefCode=" . $refCode . ";");
-       return self::getLastProgram($playlistNumber);
-   }
+        $sql = $this->db->query("SELECT PlaylistId FROM playlist WHERE RefCode=" . $refCode . ";");
+        while ($row = $sql->fetch(\PDO::FETCH_ASSOC))
+            $playlistNumber = $row['PlaylistId'];
+        $this->db->query("UPDATE library SET missing=1 WHERE RefCode=" . $refCode . ";");
+        return self::getLastProgram($playlistNumber);
+    }
 
-     /*
+    /*
     * @abstract Get the last program of the missing album
     * @param int $playlistNumber The album's unique playlist number
     */
-     public function getLastProgram($playlistNumber) {
+    public function getLastProgram($playlistNumber) {
         $sql = $this->db->query("SELECT programname, date FROM song WHERE playlistnumber=" . $playlistNumber .  " ORDER BY date DESC LIMIT 1;");
         $lastProgram = [];
         while ($row = $sql->fetch(\PDO::FETCH_ASSOC))
@@ -330,15 +330,13 @@ class playlist extends TPS{
         return $lastProgram;
     }
 
-
-
     /*
     * @abstract Marks the album as found in the database
     * @param int $refCode The album's unique RefCode
     */
     public function setToFound($refCode) {
-       $this->db->query("UPDATE library SET missing=0 WHERE RefCode=" . $refCode . ";");
-   }
+        $this->db->query("UPDATE library SET missing=0 WHERE RefCode=" . $refCode . ";");
+    }
 
     /*
     * @abstract Return the top 40 ranked albums for the given time period
@@ -347,74 +345,74 @@ class playlist extends TPS{
     * @return array of dictionaries containing the top 40 albums information (or less if <40 albums played)
     */
     public function getTop40($startDate, $endDate) {
-       $startDate = new \DateTime($startDate);
-       $endDate = new \DateTime($endDate);
+        $startDate = new \DateTime($startDate);
+        $endDate = new \DateTime($endDate);
 
-       $oneWeekStart = clone $endDate;
-       $oneWeekStart->modify('-7 days');
+        $oneWeekStart = clone $endDate;
+        $oneWeekStart->modify('-7 days');
 
-       $twoWeekStart = clone $oneWeekStart;
-       $twoWeekStart->modify('-7 days');
+        $twoWeekStart = clone $oneWeekStart;
+        $twoWeekStart->modify('-7 days');
 
-       $threeWeekStart = clone $twoWeekStart;
-       $threeWeekStart->modify('-7 days');
+        $threeWeekStart = clone $twoWeekStart;
+        $threeWeekStart->modify('-7 days');
 
-       $fourWeekStart = clone $threeWeekStart;
-       $fourWeekStart->modify('-7 days');
+        $fourWeekStart = clone $threeWeekStart;
+        $fourWeekStart->modify('-7 days');
 
-       $sql = $this->db->query("SELECT playlist.SmallCode, library.release_date, library.rating, library.Locale, playlist.Activate " .
-        "FROM library LEFT JOIN playlist ON library.RefCode=playlist.RefCode WHERE library.RefCode IN " .
-        "(SELECT RefCode FROM playlist WHERE SmallCode IN (SELECT playlistnumber FROM song " .
-        "WHERE playlistnumber IS NOT NULL AND date >= '" . $startDate->format('Y-m-d') . "' AND date <= '" . $endDate->format('Y-m-d') . "'));");
-       $albumInfo = [];
-       while ($row = $sql->fetch(\PDO::FETCH_ASSOC))
-           array_push($albumInfo, $row);
+        $sql = $this->db->query("SELECT playlist.SmallCode, library.release_date, library.rating, library.Locale, playlist.Activate " .
+        			"FROM library LEFT JOIN playlist ON library.RefCode=playlist.RefCode WHERE library.RefCode IN " .
+        			"(SELECT RefCode FROM playlist WHERE SmallCode IN (SELECT playlistnumber FROM song " .
+        			"WHERE playlistnumber IS NOT NULL AND date >= '" . $startDate->format('Y-m-d') . "' AND date <= '" . $endDate->format('Y-m-d') . "'));");
+        $albumInfo = [];
+        while ($row = $sql->fetch(\PDO::FETCH_ASSOC))
+            array_push($albumInfo, $row);
 
-       $sql = $this->db->query("SELECT programname, playlistnumber as SmallCode, " .
-        "SUM(IF(date > '" .$oneWeekStart->format('Y-m-d') . "' AND date <= '" . $endDate->format('Y-m-d') . "', 1, 0)) as 1wk, " .
-        "SUM(IF(date > '" . $twoWeekStart->format('Y-m-d') . "' AND date <= '" . $oneWeekStart->format('Y-m-d') . "', 1, 0)) as 2wk, " .
-        "SUM(IF(date > '" . $threeWeekStart->format('Y-m-d') . "' AND date <= '" . $twoWeekStart->format('Y-m-d') . "', 1, 0)) as 3wk, " .
-        "SUM(IF(date > '" . $fourWeekStart->format('Y-m-d') . "' AND date <= '" . $threeWeekStart->format('Y-m-d') . "', 1, 0)) as 4wk " .
-        "FROM song WHERE playlistnumber IS NOT NULL GROUP BY programname, SmallCode;");
-       $albumPlays = [];
-       while ($row = $sql->fetch(\PDO::FETCH_ASSOC))
-           array_push($albumPlays, $row);
+        $sql = $this->db->query("SELECT programname, playlistnumber as SmallCode, " .
+        			"SUM(IF(date > '" .$oneWeekStart->format('Y-m-d') . "' AND date <= '" . $endDate->format('Y-m-d') . "', 1, 0)) as 1wk, " .
+			        "SUM(IF(date > '" . $twoWeekStart->format('Y-m-d') . "' AND date <= '" . $oneWeekStart->format('Y-m-d') . "', 1, 0)) as 2wk, " .
+			        "SUM(IF(date > '" . $threeWeekStart->format('Y-m-d') . "' AND date <= '" . $twoWeekStart->format('Y-m-d') . "', 1, 0)) as 3wk, " .
+			        "SUM(IF(date > '" . $fourWeekStart->format('Y-m-d') . "' AND date <= '" . $threeWeekStart->format('Y-m-d') . "', 1, 0)) as 4wk " .
+			        "FROM song WHERE playlistnumber IS NOT NULL GROUP BY programname, SmallCode;");
+        $albumPlays = [];
+        while ($row = $sql->fetch(\PDO::FETCH_ASSOC))
+            array_push($albumPlays, $row);
 
-       foreach ($albumInfo as &$album) {
+        foreach ($albumInfo as &$album) {
 	    // Assign week x play count
-           $lastWeekPlays  = 0;
-           $twoWeekPlays   = 0;
-           $threeWeekPlays = 0;
-           $fourWeekPlays  = 0;
+            $lastWeekPlays  = 0;
+            $twoWeekPlays   = 0;
+            $threeWeekPlays = 0;
+            $fourWeekPlays  = 0;
 
-           foreach ($albumPlays as $plays)
-              if ($plays['SmallCode'] == $album['SmallCode']) {
-                  $lastWeekPlays  += $plays['1wk'];
-                  $twoWeekPlays   += $plays['2wk'];
-                  $threeWeekPlays += $plays['3wk'];
-                  $fourWeekPlays  += $plays['4wk'];
-              }
+            foreach ($albumPlays as $plays)
+                if ($plays['SmallCode'] == $album['SmallCode']) {
+                    $lastWeekPlays  += $plays['1wk'];
+                    $twoWeekPlays   += $plays['2wk'];
+                    $threeWeekPlays += $plays['3wk'];
+                    $fourWeekPlays  += $plays['4wk'];
+                }
 
 	    // Assign recentCode
-              if ($lastWeekPlays > 0)
-                 $recentCode = 3;
-             elseif ($twoWeekPlays > 0)
-              $recentCode = 1.5;
-          elseif ($threeWeekPlays > 0)
-              $recentCode = 1.1;
-          else
-              $recentCode = 1;
+            if ($lastWeekPlays > 0)
+                $recentCode = 3;
+            elseif ($twoWeekPlays > 0)
+                $recentCode = 1.5;
+	    elseif ($threeWeekPlays > 0)
+                $recentCode = 1.1;
+            else
+                $recentCode = 1;
 
 	    // Assign previousWeeksPlayScore
-          if ($twoWeekPlays > 0)
-              $score = 3;
-          elseif ($threeWeekPlays > 0)
-              $score = 2;
-          elseif ($fourWeekPlays > 0)
-              $score = 1;
-          else
-              $score = 0;
-          $previousWeeksPlayScore = $score;
+            if ($twoWeekPlays > 0)
+                $score = 3;
+            elseif ($threeWeekPlays > 0)
+                $score = 2;
+            elseif ($fourWeekPlays > 0)
+                $score = 1;
+            else
+                $score = 0;
+            $previousWeeksPlayScore = $score;
 
 	    // Assign playlistCode
 	    $activDate = new \DateTime($album['Activate']); // To include the 3rd, set as midnight of 4th?
@@ -435,17 +433,17 @@ class playlist extends TPS{
 	    // Assign totalScore
 	    $noPlays = $lastWeekPlays + $twoWeekPlays + $threeWeekPlays + $fourWeekPlays == 0;
 	    if ($noPlays)
-          $totalScore = 0;
-      else
-          $totalScore = pow($lastWeekPlays + 1, 2) * $numShow * $recentCode * $playlistCode * pow($releaseCode, 2) * $numDJs * $rateAmount + $locationCode + $previousWeeksPlayScore;
-      $album['totalScore'] = $totalScore;
-  }
+                $totalScore = 0;
+            else
+                $totalScore = pow($lastWeekPlays + 1, 2) * $numShow * $recentCode * $playlistCode * pow($releaseCode, 2) * $numDJs * $rateAmount + $locationCode + $previousWeeksPlayScore;
+            $album['totalScore'] = $totalScore;
+        }
 
 	// Return the top 40 albums (or less if <40 albums in $albumInfo)
-  usort($albumInfo, array($this, 'sortByTotalScore'));
-  $top40 = array_slice($albumInfo, 0, 40);
-  return json_encode($top40);
-}
+        usort($albumInfo, array($this, 'sortByTotalScore'));
+        $top40 = array_slice($albumInfo, 0, 40);
+        return json_encode($top40);
+    }
 
     /*
     * @abstract A helper function to determine the playlist code of a top 40 album
@@ -454,16 +452,16 @@ class playlist extends TPS{
     * @return int The playlist code of the given album
     */
     private function getPlaylistCode($activDate, $endDate) {
-       $weeksOnPL = $endDate->diff($activDate)->d/7;
-       if ($weeksOnPL <= 1)
-          return 2;
-      elseif ($weeksOnPL <= 2)
-          return 1.5;
-      elseif ($weeksOnPL <= 3)
-          return 1.2;
-      else
-          return 1;
-  }
+        $weeksOnPL = $endDate->diff($activDate)->d/7;
+        if ($weeksOnPL <= 1)
+            return 2;
+        elseif ($weeksOnPL <= 2)
+            return 1.5;
+        elseif ($weeksOnPL <= 3)
+            return 1.2;
+        else
+            return 1;
+    }
 
     /*
     * @abstract A helper function to determine the release code of a top 40 album
@@ -472,25 +470,25 @@ class playlist extends TPS{
     * @return int The release code of the given album
     */
     private function getReleaseCode($releaseDate, $endDate) {
-       $daysOld = (int)$releaseDate->diff($endDate)->format('%r%a');
+        $daysOld = (int)$releaseDate->diff($endDate)->format('%r%a');
 	$dateDiff = max($daysOld, 1); // Unreleased or today-released albums can't be negative
 	if ($dateDiff <= 40)
-       return 5;
-   elseif ($dateDiff < 60)
-       return 3;
-   elseif ($dateDiff < 80)
-       return 2.5;
-   elseif ($dateDiff < 100)
-       return 2;
-   elseif ($dateDiff < 130)
-       return 1.5;
-   elseif ($dateDiff < 150)
-       return 1.2;
-   elseif ($dateDiff < 180)
-       return 1.1;
-   else
-       return 1;
-}
+            return 5;
+        elseif ($dateDiff < 60)
+            return 3;
+        elseif ($dateDiff < 80)
+            return 2.5;
+        elseif ($dateDiff < 100)
+            return 2;
+        elseif ($dateDiff < 130)
+            return 1.5;
+        elseif ($dateDiff < 150)
+            return 1.2;
+        elseif ($dateDiff < 180)
+            return 1.1;
+        else
+            return 1;
+    }
 
     /*
     * @abstract A helper function to convert rating to a weighted rate amount
@@ -498,16 +496,16 @@ class playlist extends TPS{
     * @return int The rate amount of the given rating
     */
     private function getRateAmount($rating) {
-       if ($rating == 2)
-           return 1;
-       elseif ($rating == 3)
-           return 1.5;
-       elseif ($rating == 4)
-           return 2.5;
-       elseif ($rating == 5)
-           return 3;
-       else
-           return 0;
+        if ($rating == 2)
+            return 1;
+        elseif ($rating == 3)
+            return 1.5;
+        elseif ($rating == 4)
+            return 2.5;
+        elseif ($rating == 5)
+            return 3;
+        else
+            return 0;
    }
 
     /*
@@ -517,18 +515,18 @@ class playlist extends TPS{
     * @return int The weighted number of DJs that played the album
     */
     private function getNumDJs($numShow, $rateAmount) {
-       if ($numShow == 0)
-           return 0;
-       elseif ($rateAmount == 3 || $rateAmount == 2.5)
-           return $numShow + 1;
-       elseif ($rateAmount == 1.5)
-           return $numShow;
-       elseif ($rateAmount == 1 && $numShow == 0.5)
-           return 1;
-       elseif ($rateAmount == 1)
-           return $numShow - 0.5;
-       else
-           return 0;
+        if ($numShow == 0)
+            return 0;
+        elseif ($rateAmount == 3 || $rateAmount == 2.5)
+            return $numShow + 1;
+        elseif ($rateAmount == 1.5)
+            return $numShow;
+        elseif ($rateAmount == 1 && $numShow == 0.5)
+            return 1;
+        elseif ($rateAmount == 1)
+            return $numShow - 0.5;
+        else
+            return 0;
    }
 
     /*
@@ -538,17 +536,17 @@ class playlist extends TPS{
     * @return int The location code of the given locale
     */
     private function getLocationCode($locale, $smallCode) {
-       if ($locale == 'International')
-           return 1;
-       elseif ($locale == 'Country')
-           return 2;
-       elseif ($locale == 'Province')
-           return 3;
-       elseif ($locale == 'Local')
-           return 4;
-       else
-           throw new Exception("Invalid locale assigned to album. (SmallCode: " . $smallCode . ")");
-   }
+        if ($locale == 'International')
+            return 1;
+        elseif ($locale == 'Country')
+            return 2;
+        elseif ($locale == 'Province')
+            return 3;
+        elseif ($locale == 'Local')
+            return 4;
+        else
+            throw new Exception("Invalid locale assigned to album. (SmallCode: " . $smallCode . ")");
+    }
 
     /*
     * @abstract A helper function to sort albums by their totalScore
@@ -599,21 +597,21 @@ class playlist extends TPS{
 
 
     public function setExpiry($playlistIds, $date){
-     if(!is_array($playlistIds)){
-        $playlistIds = array($playlistIds);
-    }
-    $param = NULL;
-    $stmt = $this->db->prepare("UPDATE playlist SET `Expire`=:date"
-        . " WHERE PlaylistId=:param");
-    $stmt->bindParam(":param", $param);
-    $stmt->bindParam(":date", $date);
-    foreach($playlistIds as $param){
-        if(!$stmt->execute()){
-            throw new Exception($stmt->errorInfo());
+        if(!is_array($playlistIds)){
+            $playlistIds = array($playlistIds);
         }
+        $param = NULL;
+        $stmt = $this->db->prepare("UPDATE playlist SET `Expire`=:date"
+        			. " WHERE PlaylistId=:param");
+        $stmt->bindParam(":param", $param);
+        $stmt->bindParam(":date", $date);
+        foreach($playlistIds as $param){
+            if(!$stmt->execute()){
+                throw new Exception($stmt->errorInfo());
+            }
+        }
+        return true;
     }
-    return true;
-}
 
 public function setStart($playlistIds, $date){
     if(!is_array($playlistIds)){
