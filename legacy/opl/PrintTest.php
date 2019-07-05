555a099b0fe4451b1772ead356c2b695665c457d
Derek Melchin <melchin.derek@gmail.com>
2019-07-05 23:49:39 +0000

    Design the printed CD cover with all the requested data

--- legacy/opl/PrintTest.php
+++ legacy/opl/PrintTest.php
@@ -10,6 +10,7 @@ $type = filter_input(INPUT_GET,'type', FILTER_SANITIZE_NUMBER_INT)?:5160;
 $indent = filter_input(INPUT_GET,'start', FILTER_SANITIZE_NUMBER_INT) ?: 0;
 $outline = filter_input(INPUT_GET,'outline',FILTER_SANITIZE_STRING) ?: 'false';
 $library = new \TPS\library();
+$reviews = new \TPS\reviews();
 ?>
 <html lang="en">
 <head>
@@ -42,6 +43,7 @@ $library = new \TPS\library();
             background-image: none;
             color:#000000
         }
+        -webkit-print-color-adjust: exact;
     }
 
     @page :right{
@@ -55,15 +57,53 @@ $library = new \TPS\library();
         background-color: orange;
         text-align: center;
     }
+
+    .cd_cover_row {
+        display: flex;
+        margin-top: 10px;
+        font-size: 9px;
+    }
+
+    .cd_cover_tile {
+        border: 1px solid darkgrey;
+        flex: 1;
+        margin: 0 5px;
+    }
+
+    .cd_cover_tile_header {
+        color: white;
+        background-color: grey;
+	padding: 1px;
+	font-weight: bold;
+    }
+
+    .cd_cover_tile_body {
+        text-align: left;
+	margin: 5px;
+    }
+
     </style>
 </head>
 <body>
     <div class="no-print">Please use top and bottom margin of 0.5" and 0.0" sides. some printers may vary, adjust as needed</div>
     <?php
+	function buildTile($title, $info) {
+	    $tile = "<div class='cd_cover_tile'>" .
+			"<div class='cd_cover_tile_header'>" . $title . "</div>" .
+			"<div class='cd_cover_tile_body'>";
+	    if (is_array($info))
+		$tile .= implode("", array_map(function($i) {return '-'.$i.'<br />';}, $info));
+	    else
+		$tile .= $info;
+	    $tile .=    "</div>" .
+		    "</div>";
+	    return $tile;
+	}
+
         for($i=1;$i<$indent;$i++){
             echo "<div class=\"label\" style=\"border: 1px dotted black;\"></div>";
         }
-        foreach($_SESSION['PRINTID'] as $BCD){
+        foreach($_SESSION['PRINTID'] as $i=>$BCD){
             $albums = $library->getAlbumByRefcode($BCD['RefCode'], TRUE);
             if(sizeof($albums)<1){
                 continue;
@@ -82,6 +122,22 @@ $library = new \TPS\library();
 	    $recordLabels = $library->getLabelsByRefCode($RefCode);
 	    $tags = $library->getTagsByRefCode($RefCode);
 
+	    $reviewIds = $reviews->getReviewIdsByRefCode($RefCode);
+	    $recommendations = "";
+	    $descriptions = [];
+	    $notes = [];
+	    foreach ($reviewIds as $reviewId) {
+		$fullReview = $reviews->getFullReview($reviewId);
+		$recommendations .= $fullReview['review']['recommendations'];
+		array_push($descriptions, $fullReview['review']['description']);
+		array_push($notes, $fullReview['review']['notes']);
+	    }
+
+	    // Convert the string of recommendations into an array
+	    $recommendations = explode(",", $recommendations);
+	    foreach ($recommendations as &$rec)
+		$rec = trim($rec);
+
 	    $recordLabelNames = array_map(function($label) {return $label['Name'];}, $recordLabels);
 
             #$library->createBarcode($RefCode);
@@ -95,7 +151,10 @@ $library = new \TPS\library();
             $padded= join('', array($genreNum, str_pad($BCD['RefCode'], 11 - strlen($genreNum), "0", STR_PAD_LEFT)));
 
             //echo "<img src='barcode/createBarcode.php?bcd=$BCD'/>";
-            echo "<div class=\"label\" style=\"border: 1px dotted black; width: 475px; height: 475px;\">";
+            echo "<div class=\"label\" style=\"border: 1px dotted black; width: 418px; height: 436px; float: none; margin-left:0.22in;";
+	    if ($i % 2 == 0)
+		echo "margin-top:0.5in;";
+	    echo "\">";
             echo "<span><img style='float:left; margin:0px;' src='../legacy/opl/barcode/barcode.php?bcd=$padded' alt='$padded'/>";
             if($locale=="Country"){
                 echo "<img style='float: left; margin: 0px;' src='../legacy/opl/maple.gif' alt='CC'/>";
@@ -126,12 +185,26 @@ $library = new \TPS\library();
 		 "<i style='float:left; font-size: small;'>" . substr($album,0,20) . $albpost."</i>" .
 		 "<span style='float:right;'>" . $genre . "</span>" . 
 		 "<br style='clear: both'/>" .
-		 "<p>" . implode(" & ", $hometowns) . "</p>" .
-		 "<p>" . implode(" & ", $subgenres) . "</p>" .
-		 "<p>" . $libraryCode . "</p>" .
-		 "<p>" . implode(" & ", $recordLabelNames) . "</p>" .
-		 "<p>" . implode(" & ", $tags) . "</p>" .
+		 "<div class='cd_cover_row'>" .
+			buildTile('Library Code', $libraryCode) .
+			buildTile('Record Labels', $recordLabelNames) .
+			buildTile('Hometowns', $hometowns) .
+		 "</div>" .
+		 "<div class='cd_cover_row'>" .
+			buildTile('Subgenres', $subgenres) .
+			buildTile('RIYL', $recommendations) .
+			buildTile('Tags', $tags) .
+		 "</div>" .
+		 "<div class='cd_cover_row'>" .
+			buildTile('Description', $descriptions) .
+		 "</div>" .
+		 "<div class='cd_cover_row'>" .
+			buildTile('Notes', $notes) .
+		 "</div>" .
 		 "</div>";
+	    if ($i % 2 == 1) {
+		echo "<p style='page-break-before: always'></p><div style='height:0.1px'></div>";
+	    }
         }
     ?>
 <div class="page-break"></div>
