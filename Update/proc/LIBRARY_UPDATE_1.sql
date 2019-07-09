01121ddc7b793c7727636637c3500e19a7c7aa29
Derek Melchin <melchin.derek@gmail.com>
2019-07-09 23:13:52 +0000

    Remove redundant update

--- Update/proc/LIBRARY_UPDATE_1.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `permissions` 
-ADD COLUMN `Library_View` INT(1) NULL DEFAULT 0 AFTER `Genre_Edit`,
-ADD COLUMN `Library_Edit` INT(1) NULL DEFAULT 0 AFTER `Library_View`,
-ADD COLUMN `Library_Create` INT(1) NULL DEFAULT 0 AFTER `Library_Edit`;
