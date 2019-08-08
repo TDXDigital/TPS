3c66ac70a1f8d608e87973a31f0b805f9d9fb12f
Derek Melchin <melchin.derek@gmail.com>
2019-08-08 19:01:21 +0000

    Insert library label info into int table before dropping column

--- Update/proc/LIBRARY_UPDATE_19.sql
+++ Update/proc/LIBRARY_UPDATE_19.sql
@@ -1,3 +1,4 @@
+INSERT INTO library_recordlabel (SELECT RefCode, labelid FROM library);||
 ALTER TABLE `library` DROP FOREIGN KEY `Record`;||
 ALTER TABLE `library` DROP INDEX `Label_idx`;||
 ALTER TABLE `library` DROP COLUMN `labelid`;
