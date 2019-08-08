44c3a36b66a85398b28efc9e0319c87e467137a7
Derek Melchin <melchin.derek@gmail.com>
2019-08-08 00:13:15 +0000

    Fix EPIS-001 update bug with dumped data

--- Update/proc/EPISODE_UPDATE_1.sql
+++ Update/proc/EPISODE_UPDATE_1.sql
@@ -1,2 +1,5 @@
+ALTER TABLE episode ALTER COLUMN LastAccess SET DEFAULT '1970-01-01 00:00:01';||
+UPDATE episode SET LastAccess='1970-01-01 00:00:01' WHERE LastAccess < '1000-01-01';||
 ALTER TABLE episode DROP FOREIGN KEY Program;||
 ALTER TABLE episode ADD CONSTRAINT Program FOREIGN KEY (callsign, programname) REFERENCES program (callsign, programname) ON DELETE CASCADE ON UPDATE CASCADE;
+
