b1ac98876010bee8d5cd654f11460aaae0c332b7
Derek Melchin <melchin.derek@gmail.com>
2019-08-08 19:24:27 +0000

    Move recordlabel info to int table before deleting it

--- Update/proc/LIBRARY_UPDATE_19.sql
+++ Update/proc/LIBRARY_UPDATE_19.sql
@@ -1,4 +1,9 @@
 INSERT INTO library_recordlabel (SELECT RefCode, labelid FROM library);||
+UPDATE library_recordlabel 
+  SET recordlabel_LabelNumber=(SELECT LabelNumber FROM recordlabel WHERE Name='Self-Released') 
+  WHERE recordlabel_LabelNumber IN (SELECT LabelNumber FROM recordlabel WHERE Name IN ('Self Released', 'Independent', 'SR'));||
+UPDATE recordlabel SET name_alias_duplicate=NULL WHERE Name='Self-Released';||
 ALTER TABLE `library` DROP FOREIGN KEY `Record`;||
 ALTER TABLE `library` DROP INDEX `Label_idx`;||
-ALTER TABLE `library` DROP COLUMN `labelid`;
+ALTER TABLE `library` DROP COLUMN `labelid`;||
+DELETE FROM recordlabel WHERE Name IN ('Self Released', 'Independent', 'SR');
